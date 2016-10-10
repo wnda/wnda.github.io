532a2d613bf8d7da81a85f54405fb0ac2ac88d9b
A. M. Douglas <me@amdouglas.com>
2016-10-10 21:50:34 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-self.importScripts( './assets/js/serviceworker-cache-polyfill.js' );
+self.importScripts( 'https://amdouglas.com/assets/js/serviceworker-cache-polyfill.js' );
 
 var CACHE_VERSION = 11;
 var CURRENT_CACHES = {
@@ -9,14 +9,16 @@ var CURRENT_CACHES = {
 
 self.addEventListener('install', function(event) {
   var urlsToPrefetch = [
-    './',
-    './assets/css/main.css',
-    './assets/css/icons.css',
-    './assets/js/serviceworker-cache-polyfill.js',
-    './assets/img/favicon.ico',
-    'https://dev.amdouglas.com/manifest.json',
-    'https://fonts.googleapis.com/css?family=Cabin:700',
-    'https://fonts.gstatic.com/s/cabin/v9/82B-3YlzWJm8zbCrVEmc_vesZW2xOQ-xsNqO47m55DA.woff2'
+    'https://amdouglas.com/',
+    'https://amdouglas.com/404',
+    'https://amdouglas.com/assets/css/main.css',
+    'https://amdouglas.com/assets/css/fonts.css',
+    'https://amdouglas.com/assets/js/app.js',
+    'https://amdouglas.com/assets/js/serviceworker-cache-polyfill.js',
+    'https://amdouglas.com/assets/img/favicon.ico',
+    'https://amdouglas.com/assets/fonts/Rubik-Regular.woff2', // if a browser doesn't support woff2, it certainly won't support SW
+    'https://amdouglas.com/assets/fonts/Rubik-Bold.woff2',
+    'https://amdouglas.com/manifest.json'
   ];
   console.log('Handling install event. Resources to prefetch:', urlsToPrefetch);
   self.skipWaiting();
@@ -46,53 +48,16 @@ self.addEventListener('activate', function(event){
 });
 
 self.addEventListener('fetch', function(event){
-  console.log('Handling fetch event for', event.request.url);
-  if(event.request.headers.get('range')){
-    var pos =
-    Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
-    console.log('Range request for', event.request.url,
-      ', starting position:', pos);
-    event.respondWith(
-      caches.open(CURRENT_CACHES.prefetch)
-      .then(function(cache) {
-        return cache.match(event.request.url);
-      }).then(function(res) {
-        if (!res) {
-          return fetch(event.request)
-          .then(res => {
-            return res.arrayBuffer();
-          });
-        }
-        return res.arrayBuffer();
-      }).then(function(ab) {
-        return new Response(
-          ab.slice(pos),
-          {
-            status: 206,
-            statusText: 'Partial Content',
-            headers: [
-              // ['Content-Type', 'video/webm'],
-              ['Content-Range', 'bytes ' + pos + '-' +
-                (ab.byteLength - 1) + '/' + ab.byteLength]]
-          });
-      }));
-  } else {
-    console.log('Non-range request for', event.request.url);
-    event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        console.log('Found response in cache:', response);
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      if(response){
         return response;
       }
-      console.log('No response found in cache. About to fetch from network...');
       return fetch(event.request).then(function(response) {
-        console.log('Response from network is:', response);
         return response;
       }).catch(function(error) {
         console.error('Fetching failed:', error);
         throw error;
       });
-    })
-    );
-  }
+    }));
 });
