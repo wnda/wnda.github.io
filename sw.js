5d07529501ad9fa1cbab0a4c2b510c901ad43520
A. M. Douglas <me@amdouglas.com>
2016-10-22 19:07:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 self.importScripts('https://amdouglas.com/assets/js/serviceworker-cache-polyfill.js');
 
-var CACHE_VERSION = 1;
+var CACHE_VERSION = 3;
 var CURRENT_CACHES = {
   prefetch: 'pfc-v' + CACHE_VERSION
 };
