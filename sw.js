a78f3ccb724a90920c2a917b7e0c9a9a45de870d
A. M. Douglas <me@amdouglas.com>
2016-11-03 06:35:38 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 self.importScripts('https://amdouglas.com/assets/js/serviceworker-cache-polyfill.js');
 
-var CACHE_VERSION = 16;
+var CACHE_VERSION = 17;
 var CURRENT_CACHES = {
   prefetch: 'amd-v' + CACHE_VERSION
 };
